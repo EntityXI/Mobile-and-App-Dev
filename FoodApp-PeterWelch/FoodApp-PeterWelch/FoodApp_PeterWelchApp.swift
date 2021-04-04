c64bd566e326c3b7e58d3ab98078bd5976422076
EntityXI <peter.welch@griffithuni.edu.au>
2021-04-05 08:32:18 +1000

    rename project for milstone 2

---
+++ FoodApp-PeterWelch/FoodApp-PeterWelch/FoodApp_PeterWelchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp_PeterWelchApp.swift
+//  FoodApp-PeterWelch
+//
+//  Created by Pete Welch on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp_PeterWelchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
